2e2bdae845f96092f36cfde53587dc0c72be54a6
Cristian Lupu <cristi@thenoughtyfox.com>
2026-05-11 17:30:25 +0300

    chore: bump version to 2.1.5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.7
 import PackageDescription
 
-let version = "2.1.4"
-let checksum = "d6cfe0bc04927a3822fcd1523764e306ac2aaef14064ad33c38e87eb3a86aeb4"
+let version = "2.1.5"
+let checksum = "60fd3c7c4252fdf5e3ce367b3317903d4d57e1e5ee546bc1985664388130065e"
 
 let package = Package(
     name: "ArpalusSDK",
